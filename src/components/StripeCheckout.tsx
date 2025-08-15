ac7760760dafa79c8afaf869ce451241656a3384
brian <88lightningcreations@gmail.com>
2025-08-15 18:09:13 -0500

    productID logic made , i.e. handler method

--- src/components/StripeCheckout.tsx
+++ src/components/StripeCheckout.tsx
@@ -3,12 +3,21 @@ import { Button } from 'react-bootstrap';
 
 interface Props {
   productId: string;
-  onCheckout: () => void;
+  onCheckout: (productId: string) => void;
 }
 
 export default function StripeCheckout({ productId, onCheckout }: Props) {
+  // Use productId prop in the checkout handler
+  const handleClick = () => {
+    if (productId) {
+      onCheckout(productId);
+    } else {
+      alert('No product selected for checkout.');
+    }
+  };
+
   return (
-    <Button variant="success" onClick={onCheckout}>
+    <Button variant="success" onClick={handleClick}>
       Buy Now
     </Button>
   );
