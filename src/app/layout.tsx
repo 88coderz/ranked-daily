9141138017fbf57aa74874774ed77a2ffe6c189b
88lightningcreations <88lightningcreations@gmail.com>
2025-10-16 15:26:46 +0000

    gtm shim added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './globals.css'
+import './globals.css';
 import Header from '@/components/Header';
 import Consent from '@/components/Consent';
 import Script from 'next/script';
@@ -10,7 +10,7 @@ export const metadata = {
   title: 'Ranked Daily',
   description: 'A platform for ranking and discussing daily topics.',
   keywords: 'us news, global news, tech, marketing, skilled labor, products, articles, rankings, daily'
-}
+};
 
 export default function RootLayout({
   children,
@@ -20,20 +20,26 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager Shim and Default Consent */}
+        <Script id="gtag-shim" strategy="beforeInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('consent', 'default', {
+              'ad_storage': 'denied',
+              'analytics_storage': 'denied',
+              'ad_user_data': 'denied',
+              'ad_personalization': 'denied'
+            });
+          `}
+        </Script>
+        {/* Google Tag Manager */}
         <Script id="google-tag-manager" strategy="afterInteractive">
           {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-              w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-              var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-              // Set default consent to 'denied' before GTM script loads
-              window.gtag('consent', 'default', {
-                'ad_storage': 'denied',
-                'analytics_storage': 'denied',
-                'ad_user_data': 'denied',
-                'ad_personalization': 'denied',
-              });
-              f.parentNode.insertBefore(j,f);
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${GTM_ID}');
           `}
         </Script>
@@ -54,5 +60,5 @@ export default function RootLayout({
         <Consent />
       </body>
     </html>
-  )
+  );
 }
